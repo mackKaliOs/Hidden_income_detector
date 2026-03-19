b5db14325c66354ab817a01d8ea558ddaddbe18f
Kayleighy Mackintosh <145639600+mackKaliOs@users.noreply.github.com>
2026-03-19 18:58:14 -0400

    Create src/framework.py with AnalysisFramework class and model structures

---
+++ src/framework.py
@@ -0,0 +1,19 @@
+class AnalysisFramework:
+    def __init__(self):
+        pass
+
+    class DC:
+        def __init__(self):
+            pass
+
+    class BR:
+        def __init__(self):
+            pass
+
+    class CA:
+        def __init__(self):
+            pass
+
+    class EB:
+        def __init__(self):
+            pass
